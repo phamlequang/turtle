d64e77a3a9031e49df3353e264734fe691016b7a
Pham Le Quang <phamlequang@gmail.com>
2018-12-15 23:57:45 +0100

    split logic into smaller functions and handle errors properly

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,70 @@
+use std::ffi::OsStr;
 use std::io;
 use std::io::Write;
+use std::process;
 
-pub const QUIT: &str = "quit";
-pub const EXIT: &str = "exit";
+const TURTLE: &str = "turtle > ";
+const EMPTY: &str = "";
+const QUIT: &str = "quit";
+const EXIT: &str = "exit";
 
+// Run turtle shell
 pub fn run() {
     let mut quit = false;
-
     while !quit {
-        let command = prompt("turtle > ");
+        let command = prompt(TURTLE);
         quit = execute(&command)
     }
 }
 
+// Prompt a message to ask for a new command
 fn prompt(message: &str) -> String {
     print!("{}", message);
     io::stdout().flush().unwrap();
 
     let mut line = String::new();
-    io::stdin()
-        .read_line(&mut line)
-        .expect("failed to read line");
-
-    return line.trim().to_lowercase();
+    match io::stdin().read_line(&mut line) {
+        Ok(_) => return line.trim().to_owned(),
+        Err(e) => {
+            println!("failed to read line: {}", e);
+            return String::new();
+        }
+    }
 }
 
+// Execute a command
 fn execute(command: &str) -> bool {
     match command {
+        EMPTY => (),
         QUIT | EXIT => return true,
-        _ => println!("unknown command, please try again"),
+        _ => system(command),
     }
     return false;
+}
+
+// Run any system command as a new child process
+fn system(command: &str) {
+    let mut tokens = command.split_whitespace();
+    if let Some(program) = tokens.next() {
+        spawn(program, tokens);
+    }
+}
+
+// Spawn a program as a child process and wait for it to finish
+fn spawn<I, S>(program: &str, args: I)
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut command = process::Command::new(program);
+    command.args(args);
+
+    match command.spawn() {
+        Ok(mut child) => {
+            if let Err(e) = child.wait() {
+                println!("failed to wait for child process: {}", e);
+            }
+        }
+        Err(e) => println!("failed to execute command: {}", e),
+    }
 }
