5a6e57953a85cd5d32f58d05198b6aadd51752ea
Pham Le Quang <phamlequang@gmail.com>
2018-12-15 17:49:35 +0100

    quit and exit command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut quit = false;
+
+    while !quit {
+        print!("[turtle] > ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("failed to read line");
+
+        let command = line.trim();
+        println!("your command is: {}", command);
+
+        match command {
+            "quit" | "exit" => quit = true,
+            _ => println!("unknown command, please try again"),
+        }
+    }
 }
