6e3b07cd1a76bbc252e40d15f7cfdf89825fd87d
phamlequang <phamlequang@gmail.com>
2018-12-23 16:23:27 +0100

    split match docker machine action to a separate function

--- src/shell/instr.rs
+++ src/shell/instr.rs
@@ -1,7 +1,7 @@
 use super::cmd::Command;
 use super::docker;
 use super::git;
-use crate::config::Config;
+use crate::config::{Config, DockerMachine};
 
 #[derive(Debug)]
 pub struct Instruction {
@@ -59,29 +59,33 @@ impl Instruction {
     pub fn docker_machine(args: Vec<String>, config: &Config) -> Self {
         if let Some(machine) = &config.docker_machine {
             match args.first() {
-                Some(action) => match action.as_ref() {
-                    "create" => {
-                        let command = docker::create_machine(machine);
-                        return Self::new(vec![command], false);
-                    }
-                    "remove" => {
-                        let command = docker::remove_machine(machine);
-                        return Self::new(vec![command], false);
-                    }
-                    "restart" | "env" | "inspect" | "ip" | "kill" | "ls" | "start" | "status"
-                    | "stop" | "upgrade" | "url" | "version" => {
-                        let command = docker::do_with_machine(action, machine);
-                        return Self::new(vec![command], false);
-                    }
-                    _ => {
-                        let message = format!("--> unknown action [ {} ]", action);
-                        return Self::echo(&message);
-                    }
-                },
+                Some(action) => return Self::do_with_docker_machine(action, machine),
                 None => return Self::do_nothing(),
             }
         }
         return Self::echo("--> docker machine config is missing");
+    }
+
+    pub fn do_with_docker_machine(action: &str, machine: &DockerMachine) -> Instruction {
+        match action {
+            "create" => {
+                let command = docker::create_machine(machine);
+                return Self::new(vec![command], false);
+            }
+            "remove" => {
+                let command = docker::remove_machine(machine);
+                return Self::new(vec![command], false);
+            }
+            "restart" | "env" | "inspect" | "ip" | "kill" | "ls" | "start" | "status" | "stop"
+            | "upgrade" | "url" | "version" => {
+                let command = docker::do_with_machine(action, machine);
+                return Self::new(vec![command], false);
+            }
+            _ => {
+                let message = format!("--> unknown action [ {} ]", action);
+                return Self::echo(&message);
+            }
+        }
     }
 
     pub fn other(program: &str, args: Vec<String>) -> Self {
