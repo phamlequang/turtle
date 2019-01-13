1c0e8db64882cf1d6d357f06e00af0a9ca04d31e
phamlequang <phamlequang@gmail.com>
2019-01-13 20:46:54 +0100

    split run command into smaller functions

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -43,75 +43,55 @@ pub fn run_command(command: &Command) -> (bool, String) {
         }
     }
 
-    let raw = &command.raw;
-    let mut stdout = String::new();
-
-    if !raw.is_empty() {
-        if command.show {
-            println!("{}$ {}{}", color::Fg(Magenta), raw, style::Reset);
+    let (success, stdout) = run_raw_command(command);
+    if success {
+        if let Some(then) = &command.then {
+            return then(&stdout);
         }
+    }
 
-        let mut exec_error: Option<PopenError> = None;
-        let mut exit_status = ExitStatus::Exited(0);
+    return (success, stdout);
+}
 
-        if command.pipe {
-            let exec = subprocess::Exec::shell(raw).stdout(Pipe).stderr(Pipe);
+pub fn run_raw_command(command: &Command) -> (bool, String) {
+    let raw = &command.raw;
+    if raw.is_empty() {
+        return (true, String::new());
+    }
 
-            match exec.capture() {
-                Ok(data) => {
-                    if data.success() {
-                        stdout = data.stdout_str();
-                    } else {
-                        exit_status = data.exit_status;
-                        if !command.silent {
-                            println!("{}", data.stderr_str());
-                        }
+    if command.show {
+        println!("{}$ {}{}", color::Fg(Magenta), raw, style::Reset);
+    }
+
+    let mut exec_error: Option<PopenError> = None;
+    let mut exit_status = ExitStatus::Exited(0);
+
+    if command.pipe {
+        let exec = subprocess::Exec::shell(raw).stdout(Pipe).stderr(Pipe);
+
+        match exec.capture() {
+            Ok(data) => {
+                if data.success() {
+                    let stdout = data.stdout_str();
+                    return (true, stdout);
+                } else {
+                    exit_status = data.exit_status;
+                    if !command.silent {
+                        println!("{}", data.stderr_str());
                     }
                 }
-                Err(err) => {
-                    exec_error = Some(err);
-                }
             }
-        } else {
-            let result = subprocess::Exec::shell(raw).join();
-            match result {
-                Ok(status) => {
-                    exit_status = status;
-                }
-                Err(err) => {
-                    exec_error = Some(err);
-                }
-            }
+            Err(err) => exec_error = Some(err),
         }
-
-        if !command.silent {
-            if let Some(err) = exec_error {
-                println!(
-                    "{}--> execute error: {}{}",
-                    color::Fg(Red),
-                    err,
-                    style::Reset
-                );
-                return (false, String::new());
-            }
-
-            if !exit_status.success() {
-                println!(
-                    "{}--> failed with exit status = {:?}{}",
-                    color::Fg(Red),
-                    exit_status,
-                    style::Reset,
-                );
-                return (false, String::new());
-            }
+    } else {
+        let result = subprocess::Exec::shell(raw).join();
+        match result {
+            Ok(status) => exit_status = status,
+            Err(err) => exec_error = Some(err),
         }
     }
 
-    if let Some(then) = &command.then {
-        return then(&stdout);
-    }
-
-    return (true, stdout);
+    return handle_error(command.silent, exec_error, exit_status);
 }
 
 // Executes all commands sequentially, stop immediately in case of failure,
@@ -124,4 +104,30 @@ pub fn run_instruction(instruction: &Instruction) -> bool {
         }
     }
     return true;
+}
+
+fn handle_error(silent: bool, error: Option<PopenError>, status: ExitStatus) -> (bool, String) {
+    if !silent {
+        if let Some(err) = error {
+            println!(
+                "{}--> execute error: {}{}",
+                color::Fg(Red),
+                err,
+                style::Reset
+            );
+            return (false, String::new());
+        }
+
+        if !status.success() {
+            println!(
+                "{}--> failed with exit status = {:?}{}",
+                color::Fg(Red),
+                status,
+                style::Reset,
+            );
+            return (false, String::new());
+        }
+    }
+
+    return (true, String::new());
 }
